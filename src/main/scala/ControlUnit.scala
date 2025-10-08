ed1c435bf7da6879048f3a2497960ba03545f3bd
TristanBALDIT <tristan.baldit@gmail.com>
2025-10-08 13:12:09 +0200

    ControlUnit signals First Draft

--- src/main/scala/ControlUnit.scala
+++ src/main/scala/ControlUnit.scala
@@ -3,7 +3,11 @@ import chisel3.util._
 
 class ControlUnit extends Module {
   val io = IO(new Bundle {
-    //Define the module interface here (inputs/outputs)
+    val opcode = Input(UInt(16.W))
+    val ALU_op = Output(UInt(4.W))
+    val REG_aSel = Output(UInt(4.W))
+    val REG_bSEl = Output(UInt(4.W))
+    val REG_writeEnable = Output(Bool())
   })
 
   //Implement this module here
