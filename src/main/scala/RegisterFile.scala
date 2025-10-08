72cc3a7554b69ac74f90e7b6fe341c0d651fd20a
TristanBALDIT <tristan.baldit@gmail.com>
2025-10-08 13:04:08 +0200

    Registers First Draft

--- src/main/scala/RegisterFile.scala
+++ src/main/scala/RegisterFile.scala
@@ -3,9 +3,25 @@ import chisel3.util._
 
 class RegisterFile extends Module {
   val io = IO(new Bundle {
-    //Define the module interface here (inputs/outputs)
+    val aSel = Input(UInt(4.W))
+    val bSel = Input(UInt(4.W))
+    val writeData = Input(UInt(32.W))
+    val writeSel = Input(UInt(4.W))
+    val writeEnable = Input(Bool())
+    val a = Output(UInt(32.W))
+    val b = Output(UInt(32.W))
   })
 
-  //Implement this module here
+  // Vector of 8 registers
+  val regs = RegInit(VecInit(Seq.fill(8)(0.U(32.W))))
 
+  when(io.writeEnable) {
+    when(io.writeSel < 8.U) {
+      regs(io.writeSel) := io.writeData
+    }
+  }
+
+  // Read ports
+  io.a := Mux(io.aSel < 8.U, regs(io.aSel), 0.U)
+  io.b := Mux(io.bSel < 8.U, regs(io.bSel), 0.U)
 }
