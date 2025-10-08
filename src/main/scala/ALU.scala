00fceb87e554729921f74c3ce8122dfe15c47547
TristanBALDIT <tristan.baldit@gmail.com>
2025-10-08 10:57:27 +0200

    ALU First Draft

--- src/main/scala/ALU.scala
+++ src/main/scala/ALU.scala
@@ -1,11 +1,41 @@
 import chisel3._
 import chisel3.util._
 
+object ALUOps {
+  val add :: sub :: and :: or :: xor :: Nil = Enum(5)
+}
+
 class ALU extends Module {
   val io = IO(new Bundle {
-    //Define the module interface here (inputs/outputs)
+    val operand_1 = Input(UInt(32.W))
+    val operand_2 = Input(UInt(32.W))
+    val op = Input(UInt(3.W))
+    val result = Output(UInt(32.W))
+    val comparison_result = Output(UInt(1.W))
   })
 
-  //Implement this module here
+  io.result := 0.U
+  io.comparison_result := 0.U
 
+  switch(io.op){
+    is(ALUOps.add){
+      io.result := io.operand_1 + io.operand_2
+    }
+
+    is(ALUOps.sub){
+      io.result := io.operand_1 - io.operand_2
+    }
+
+    is(ALUOps.and){
+      io.result := io.operand_1 & io.operand_2
+    }
+
+    is(ALUOps.or){
+      io.result := io.operand_1 | io.operand_2
+    }
+
+    is(ALUOps.xor){
+      io.result := io.operand_1 ^ io.operand_2
+    }
+  }
 }
